d84f21fae650d304cebe659bd25caa4c038e3dab
erikmuar <erikmuar91@gmail.com>
2024-06-11 14:05:08 +0200

    primera parte juego cho han

--- Python/Piogram videos/Juego_ChoHan.py
+++ Python/Piogram videos/Juego_ChoHan.py
@@ -1,3 +1,27 @@
 # Juego ChoHan
+import random
 
+billetera = 10
+
+ganado = 0
+print("--- Bienvenido a Cho Han ---")
+print(f"Usted tiene {billetera}$ en la billetera")
+
+
+while billetera > 0:
+    apuesta = int(input("Haga su apuesta: "))
+    if apuesta <= billetera:
+        dado1 = random.randint(1,6)
+        dado2= random.randint(1,6)
+        total = dado1 + dado2
+        resto = total%2
+        parimp = input("Adivina, ¿Es par o impar?")
+        print(f"Salió {dado1} y {dado2} = {total} ")
+    else:
+        print("El dinero es mayor al que tiene en la billetera")
+    
+    
+    
 apuesta = int(input("Cuanto dinero deseas apostar?"))
+
+
--- Python/Piogram videos/tempCodeRunnerFile.py
+++ Python/Piogram videos/tempCodeRunnerFile.py
@@ -1,10 +1,3 @@
-num1 = int(input("Ingresa el primer número: "))
-num2 = int(input("Ingresa el segundo número: "))
-
-
-
-if (num1%num2 == 0 ):
-    print(f"¿Es {num1} divisible de {num2} ? True")
-else: 
-    print(f"¿Es {num1} divisible de {num2} ? False")
-    
+import random
+hola = random.randint(1,6)
+print(hola)
