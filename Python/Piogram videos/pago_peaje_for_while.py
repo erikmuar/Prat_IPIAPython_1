34f5647ad094f0d10aed684b0a21109965c73316
Erik <erikmuar91@gmail.com>
2024-06-14 18:56:54 +0200

    ejercicio peaje

--- Python/Piogram videos/pago_peaje_for_while.py
+++ Python/Piogram videos/pago_peaje_for_while.py
@@ -7,5 +7,32 @@
 # Total recaudado por cada tipo de automotor 
 # Cual es el tipo de Automotor que mas transita por el peaje
 
+automotores = ["Vehiculos", "Camiones","Tractomula"]
+precios= [3.5,12.00,16.00]
+contL= [0,0,0]
+acumL = [0,0,0]
+ingreso = int(input("Ingresa el numero del vehiculo: "))
 
+while ingreso != 0: 
+    if (ingreso<=len(automotores)):
+        precio = precios[ingreso-1]
+        print("Debe pagar: ",precio)
+        contL[ingreso-1]+=1
+        acumL[ingreso-1]+=precio 
+    else: 
+        print("Error de ingreso")
+        
+    ingreso = int(input("Ingresa el numero del vehiculo: "))
+    print("Total por cada tipo de automotor: ")
+for i in range(len(automotores)):
+    autos=automotores[i]
+    acum = acumL[i]
+    print(autos,":",acum)
+    
+maxi= max(contL)  
+posi = contL.index(maxi)
+autoMax = automotores[posi]
+print("El automotor que mas transita por el peaje es: ", autoMax)
 
+totalDia = sum(acumL)
+print(totalDia)
--- Python/Piogram videos/tempCodeRunnerFile.py
+++ Python/Piogram videos/tempCodeRunnerFile.py
@@ -1,67 +1,26 @@
-<<<<<<< HEAD
-# Juego Cho Han
-import random
+automotores = ["Vehiculos", "Camiones","Tractomula"]
+precios= [3.5,12.00,16.00]
+contL= [0,0,0]
+acumL = [0,0,0]
+ingreso = int(input("Ingresa el numero del vehiculo: "))
 
-billetera = 10
-ganado = 0
-continuar = "Si"
-print("--- Bienvenido a Cho Han ---")
-print(f"Tiene {billetera}$ en la billetera")
-
-apuesta = int(input("Haga su apuesta: "))
-
-while billetera > 0 and continuar == "Si":
-   
-    if apuesta <= billetera:
-        dado1 = random.randint(1,6)
-        dado2= random.randint(1,6)
-        total = dado1 + dado2
-        resto = total%2
-        parimp = input("Adivina, ¿Es par o impar?")
-        print(f"Salió {dado1} y {dado2} = {total} ")
+while ingreso != 0: 
+    if (ingreso<=len(automotores)):
+        precio = precios[ingreso-1]
+        print("Debe pagar: ",precio)
+        contL[ingreso-1]+=1
+        acumL[ingreso-1]+=precio 
+    else: 
+        print("Error de ingreso")
         
-        if resto == 0 and parimp == "par":
-            billetera += apuesta
-            ganado+=1 
-            print("Has ganado!")
-        elif resto !=0 and parimp == "impar":
-            billetera+=apuesta
-            ganado+=1
-            print("Has ganado!")
-        else: 
-            billetera-=apuesta 
-            print("Has perdido!")
-        print(f"Dinero restante en la billetera es: {billetera}")
-        
-        if billetera != 0:
-            continuar = input("¿Deseas continuar? Si o No  ")
-            apuesta = int(input("Haga su apuesta: "))            
-            
-        
-    else:
-        print("El dinero es mayor al que tiene en la billetera")
+    ingreso = int(input("Ingresa el numero del vehiculo: "))
+    print("Total por cada tipo de automotor: ")
+for i in range(len(automotores)):
+    autos=automotores[i]
+    acum = acumL[i]
+    print(autos,":",acum)
     
-print(f"Has ganado {ganado} partidas")
-print("Gracias por jugar.")
-=======
-for num in range(1,100):
-    if num > 1: 
-
-        contador = 0 
-
-        i = 2
-
-        while i < num and contador == 0: 
-            resto = num%i
-            
-            if resto == 0:
-                contador+=1
-            i +=1 
-        if contador == 0:
-            print(num)  
-      
-    
-        
- 
-    
->>>>>>> 9e6496c448088759bd490591539f3729eda104b7
+autoMas = max(contL)
+posi = contL[autoMas] 
+cocheMas = automotores[posi]
+print(autoMas)    
