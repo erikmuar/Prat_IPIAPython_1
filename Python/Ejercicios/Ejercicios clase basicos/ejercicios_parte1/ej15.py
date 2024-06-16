54436ec5991199b9fac3dee11dab494c57639e39
erikmuar <erikmuar91@gmail.com>
2024-06-16 21:49:31 +0200

    96

---
+++ Python/Ejercicios/Ejercicios clase basicos/ejercicios_parte1/ej15.py
@@ -0,0 +1,9 @@
+#Ejercicio 5
+# Construir un pequeño programa que convierta números binarios en enteros.
+#%%
+def binario_a_entero(binario:str):
+        entero = int(binario)
+        print(entero)
+    
+binario_a_entero("100110101")
+# %%
