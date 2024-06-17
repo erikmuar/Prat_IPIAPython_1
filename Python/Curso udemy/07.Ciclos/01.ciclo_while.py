6b8d7719d240591eb246f815128492580d346cbe
Erik <erikmuar91@gmail.com>
2024-06-17 16:21:26 +0200

    while udemy

---
+++ Python/Curso udemy/07.Ciclos/01.ciclo_while.py
@@ -0,0 +1,28 @@
+# Imprimir los numeros enteros del 0 al 5 utilizando while
+
+contador = 0 
+
+while contador < 6: 
+    print(contador)
+    contador +=1
+    
+else: 
+    print("Fin de ciclo")
+    
+    
+    
+    
+# Imprimir numeros con ciclo while de forma descendente 5 a 1 
+
+
+minimo = 0 
+
+contador = 5 
+
+while contador > minimo: 
+    print(contador)
+    contador -=1
+else: 
+    print("Fin de ciclo")
+    
+    
---
+++ Python/Curso udemy/07.Ciclos/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+minimo = 0 
+
+contador = 5 
+
+while contador > minimo: 
+    print(contador)
+    contador -=1
+else: 
+    print("Fin de ciclo")
