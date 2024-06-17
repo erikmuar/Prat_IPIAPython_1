725bc3d024c2068a598462478f0396ca9558a53d
Erik <erikmuar91@gmail.com>
2024-06-17 17:55:09 +0200

    strings

---
+++ Python/Udemy Python Total/Dia 1/Strings.py
@@ -0,0 +1,9 @@
+print("Me llamo \"Federico\" ") # \" \" es para poner comillas en una palabra especifica
+
+print("Esta es una linea\nY esta es otra linea")  # \n significa linea nueva
+
+print("\t Esta es la tercera linea")  # \t sirve para poner una sangria al texto 
+
+print('This isn\'t a number') # \' sirve para las apostrofes cuando escribes en ingles      
+
+print("Este signo \\ es una barra invertida") # Ponemos una segunda barra para que salga una
