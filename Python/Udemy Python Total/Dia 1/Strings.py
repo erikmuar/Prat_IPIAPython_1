195cee0663b590edaf6353976b39c1d3ab7eb557
Erik <erikmuar91@gmail.com>
2024-06-17 18:03:57 +0200

    strings final

--- Python/Udemy Python Total/Dia 1/Strings.py
+++ Python/Udemy Python Total/Dia 1/Strings.py
@@ -7,3 +7,31 @@ print("\t Esta es la tercera linea")  # \t sirve para poner una sangria al texto
 print('This isn\'t a number') # \' sirve para las apostrofes cuando escribes en ingles      
 
 print("Este signo \\ es una barra invertida") # Ponemos una segunda barra para que salga una
+
+
+#Crea un código que imprima en pantalla la siguiente expresión (pero usando print una sola vez):
+"""
+Línea 1
+Línea 2
+Línea 3
+"""
+
+print("Línea 1\nLínea 2\nLínea 3")
+
+#Crea un código que imprima en pantalla la siguiente expresión.
+"""
+A	B	C
+D	E	F
+G	H	I
+"""
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+
+#Crea un código que imprima en pantalla la siguiente expresión:
+"""
+Barra Normal: /
+Barra Invertida: \
+"""
+
+print("Barra Normal: /\nBarra Invertida: \\")
