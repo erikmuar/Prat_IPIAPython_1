958842f2691f2514c5923fa88f5a9e5f295ad134
erikmuar <erikmuar91@gmail.com>
2024-06-17 08:55:05 +0200

    ejercicio 16

--- Python/Ejercicios/Ejercicios clase basicos/ejercicios_parte1/ej16.py
+++ Python/Ejercicios/Ejercicios clase basicos/ejercicios_parte1/ej16.py
@@ -1,3 +1,8 @@
+#Escribir un pequeño programa donde:
+#- Se ingresa el año en curso.
+#- Se ingresa el nombre y el año de nacimiento de tres personas.
+#- Se calcula cuántos años cumplirán durante el año en curso.
+#- Se imprime en pantalla.
 
 import datetime
 
