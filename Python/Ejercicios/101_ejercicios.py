4d1896cc409d3b88c293e6d860c2ff0de486237d
Erik <erikmuar91@gmail.com>
2024-06-17 16:05:46 +0200

    101 1

--- Python/Ejercicios/101_ejercicios.py
+++ Python/Ejercicios/101_ejercicios.py
@@ -7,5 +7,32 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  """
- 
- 
+
+
+def imprimir():
+    for i in range(1, 100): 
+        if (i%3 == 0):
+            i = "fizz"
+            print(i)
+        elif(i%5 == 0):
+            i = "buzz"
+            print(i)
+        elif (i%3 == 0) and (i%5 == 0):
+            i = "fizzbuzz"
+            print(i)
+        else:
+            print(i)
+            
+imprimir()
+
+
+# Ejercicio 2 Es un anagrama ? 
+"""
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+"""
+        
