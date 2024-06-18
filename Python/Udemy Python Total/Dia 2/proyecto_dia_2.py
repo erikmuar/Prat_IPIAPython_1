a692878ba5618d2d0698208fbc8a60925e4830e7
erikmuar <erikmuar91@gmail.com>
2024-06-18 09:48:52 +0200

    dia 2 udemy

---
+++ Python/Udemy Python Total/Dia 2/proyecto_dia_2.py
@@ -0,0 +1,10 @@
+# Proyecto dia 2 
+
+nombre = input("Escribe tu nombre: ")
+
+ventas = float(input("Introduce cuantas ventas has hecho en Euros: "))
+
+comision = round(ventas * 0.13 ,2)
+
+
+print(f" El nombre de la persona es  {nombre}, las ventas que ha hecho han sido {ventas}€  y el porcentaje que se lleva es {comision}€")
---
+++ Python/Udemy Python Total/Dia 2/variables.py
@@ -0,0 +1,10 @@
+nombre = "Juan"
+print(nombre)
+
+
+x = 7 
+y = 2
+
+z = x//y
+
+print(z)
