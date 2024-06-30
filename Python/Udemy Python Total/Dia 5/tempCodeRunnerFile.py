38947f8a49c6708720ff6caac27c5e6b5be11712
erikmuar <erikmuar91@gmail.com>
2024-06-30 23:34:16 +0200

    dia 6

--- Python/Udemy Python Total/Dia 5/tempCodeRunnerFile.py
+++ Python/Udemy Python Total/Dia 5/tempCodeRunnerFile.py
@@ -1,38 +1,9 @@
-<<<<<<< HEAD
-def reducir_lista(lista_numeros):
+def devolver_distintos(num1,num2,num3):
     
-    lista_sin_duplicados = list(set(lista_numeros))
-    
-    print(lista_sin_duplicados)
-    
-    lista_ordenada = lista_sin_duplicados.sort()
-    
-    print(lista_ordenada)
-    
-    lista_sin_max = lista_ordenada.pop()
-    
-    print(lista_sin_max)
-    
-    return lista_sin_max
-
-reducir_lista([2,4,6,15,6,5])
-=======
-from random import *
-
-
-def lanzar_dados():
-    dado1 = randint(1,6)
-    dado2 = randint(1,6)
-    return print(dado1, dado2 )
-    
+    lista = [num1,num2,num3]
+    suma = sum(lista)
+    print (suma)
     
 
-def evaluar_jugada(dado1,dado2):
-    suma_dados = dado1+dado2
-    if suma_dados <= 6:
-        print(f"La suma de tus dados es {suma_dados}. Lamentable")
-    elif suma_dados > 6 and suma_dados< 10: 
-        print(f"La suma de tus dados es {suma_dados}. Tienes buenas chances")
-    elif suma_dados >= 10: 
-        print(f"La suma de tus dados es {suma_dados}. Parece una jugada ganadora")
->>>>>>> 86624db1e242838f2372ba54282444811d331801
+
+devolver_distintos(2,3,5)
