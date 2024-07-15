ef44d4b37c56993fffe171bdb50bb8a6f80a1a52
Erik <erikmuar91@gmail.com>
2024-07-15 17:56:00 +0200

    Atributi

--- Python/Udemy Python Total/Dia 7/atributos.py
+++ Python/Udemy Python Total/Dia 7/atributos.py
@@ -10,4 +10,70 @@ mi_pajaro = Pajaro("negro","colibri")
 
 print(f"Mi pajaro es un {mi_pajaro.especie} y es de color {mi_pajaro.color}")   
 
-print(Pajaro.alas)
+print(Pajaro.alas)  
+
+#%% Practica 1 
+#Crea una clase llamada Casa, y asígnale atributos: color, cantidad_pisos.
+
+#Crea una instancia de Casa, llamada casa_blanca, de color "blanco" y cantidad de pisos igual a 4.
+
+class Casa:
+    
+    def __init__(self,color,cantidad_pisos):
+        
+        self.color = color 
+        self.cantidad_pisos = cantidad_pisos
+        
+
+casa_blanca = Casa("blanco",4)
+
+
+#%% Practica 2 
+# Crea una clase llamada Cubo, y asígnale el atributo de clase:
+
+#caras = 6
+
+#y el atributo de instancia:
+
+#color
+
+#Crea una instancia cubo_rojo, de dicho color.
+
+class Cubo:
+    
+    caras = 6
+    
+    def __init__(self,color):
+        self.color = color
+    
+cubo_rojo = Cubo("rojo")
+
+
+#%% Práctica Atributos 3
+#Crea una clase llamada Personaje, y asígnale el siguiente atributo de clase:
+
+#real = False
+
+#Crea una instancia llamada harry_potter con los siguientes atributos de instancia:
+
+#especie = "Humano"
+
+#magico = True
+
+#edad = 17
+
+class Personaje:
+    
+    real = False
+    
+    def __init__(self,especie,magico,edad):
+        self.especie = especie
+        self.magico = magico
+        self.edad = edad 
+        
+harry_potter = Personaje("Humano","True",17)
+
+
+
+
+#%% 
