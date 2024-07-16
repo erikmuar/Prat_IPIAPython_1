5106b566577f04b71d34147c2aca41253a9a69bd
erikmuar <erikmuar91@gmail.com>
2024-07-16 11:51:01 +0200

    metodos

--- Python/Udemy Python Total/Dia 7/metodos.py
+++ Python/Udemy Python Total/Dia 7/metodos.py
@@ -17,3 +17,51 @@ piolin = Pajaro("amarillo","canario")
 piolin.piar()
 
 piolin.volar(50)
+
+
+
+#%% Practica Métodos 1 
+# Crea una clase Perro. Dicho perro debe poder ladrar.
+
+#Crea el método ladrar() y ejecútalo en una instancia de Perro. Cada vez que ladre, debe mostrarse en pantalla "Guau!".
+
+class Perro:
+    
+    def ladrar(self): 
+        print("Guau!")
+    
+
+Toby = Perro()
+
+Toby.ladrar()
+
+#%% Practica Metodos 2 
+#Crea una clase llamada Mago, y crea un método llamado lanzar_hechizo() (deberá imprimir "¡Abracadabra!").
+
+#Crea una instancia de Mago en el objeto merlin, y haz que lance un hechizo.
+
+class Mago:
+    
+    def lanzar_hechizo(self):
+        print("¡Abracadabra!")
+        
+merlin = Mago()
+
+merlin.lanzar_hechizo()
+
+
+#%% Practica Metodos 3 
+#Crea una instancia de la clase Alarma, que tenga un método llamado postergar(cantidad_minutos). El método debe imprimir en pantalla el mensaje
+
+# "La alarma ha sido pospuesta {cantidad_minutos} minutos"
+
+class Alarma: 
+    
+    def postergar(self,cantidad_minutos):
+        print(f"La alarma ha sido pospuesta {cantidad_minutos} minutos")
+
+
+reloj = Alarma()
+
+reloj.postergar(50)
+
