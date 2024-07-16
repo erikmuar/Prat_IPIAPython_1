d37dcc6744ca2df25709a254f6538dd4d62ed8a5
Erik <erikmuar91@gmail.com>
2024-07-16 15:14:14 +0200

    d

--- Python/Udemy Python Total/Dia 7/metodos.py
+++ Python/Udemy Python Total/Dia 7/metodos.py
@@ -6,7 +6,7 @@ class Pajaro:
         self.especie = especie # Atributo de instancia
     
     def piar(self): #SIEMPRE hay que poner el parametro self de forma obligatoria 
-        print("pio")
+        print(f"pio, mi color es el {self.color}")
             
     def volar(self,metros):  #SIEMPRE hay que poner el parametro self de forma obligatoria 
         print(f"El pajaro vuela {metros} metros.")
@@ -17,3 +17,35 @@ piolin = Pajaro("amarillo","canario")
 piolin.piar()
 
 piolin.volar(50)
+
+#%% Práctica Métodos 1
+#Crea una clase Perro. Dicho perro debe poder ladrar.
+
+#Crea el método ladrar() y ejecútalo en una instancia de Perro. Cada vez que ladre, debe mostrarse en pantalla "Guau!".
+
+
+class Perro:
+    
+    def ladrar(self): 
+        print("Guau!")
+    
+
+Toby = Perro()
+
+Toby.ladrar()
+
+
+
+#%% Práctica Métodos 2
+#Crea una clase llamada Mago, y crea un método llamado lanzar_hechizo() (deberá imprimir "¡Abracadabra!").
+
+#Crea una instancia de Mago en el objeto merlin, y haz que lance un hechizo.
+
+class Mago:
+    
+    def lanzar_hechizo(self):
+        print("¡Abracadabra!")
+        
+merlin = Mago()
+
+merlin.lanzar_hechizo()
