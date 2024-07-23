d74fe2530eaa2b7ceb5380ae97b4ba464d370f13
erikmuar <erikmuar91@gmail.com>
2024-07-23 12:57:01 +0200

    ejemplos

--- Python/Udemy Python Total/Dia 7/tipos_de_metodos.py
+++ Python/Udemy Python Total/Dia 7/tipos_de_metodos.py
@@ -102,3 +102,14 @@ class Pajaro:
         print("El pajaro mira")
 
 Pajaro.mirar()
+
+
+#Práctica Tipos de Métodos 2
+#Crea un método de clase revivir() que actúa sobre el atributo de clase vivo de la clase Jugador, estableciéndolo en True cada vez que es invocado. El valor predeterminado del atributo vivo, debe ser False.
+
+
+class Jugador:
+    vivo = False
+    @classmethod
+    def revivir(cls):
+        cls.vivo = True
