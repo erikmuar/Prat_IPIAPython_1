c6ca469b92f31633c62c1bb486b5a7c03e50f197
Erik <erikmuar91@gmail.com>
2024-08-02 19:06:35 +0200

    ejercicios

---
+++ SQL/sqlite_tools/crear_tabla.sql
@@ -0,0 +1,15 @@
+-- SQL
+
+CREATE TABLE IF NOT EXISTS tblUsuarios (
+   idx INTEGER PRIMARY KEY AUTOINCREMENT,
+   usuario TEXT NOT NULL,
+   nombre TEXT NOT NULL,
+   sexo TEXT CHECK(sexo IN ('M', 'F')),
+   nivel INTEGER CHECK(nivel BETWEEN 0 AND 255),
+   email TEXT,
+   telefono TEXT,
+   marca TEXT,
+   compañia TEXT,
+   saldo REAL,
+   activo INTEGER CHECK(activo IN (0, 1))
+);
